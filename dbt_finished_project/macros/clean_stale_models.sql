9ef7a15c0474f73c242acbf656f2823557a82701
Filipe Balseiro <filipe_balseiro@hakkoda.io>
2024-05-09 16:30:06 +0100

    fix: clean_stale_models added to daily workflow instead of run-end dbt hook

--- dbt_finished_project/macros/clean_stale_models.sql
+++ dbt_finished_project/macros/clean_stale_models.sql
@@ -1,4 +1,4 @@
-{% macro clean_stale_models(database=target.database, days=30, dry_run=True) %}
+{% macro clean_stale_models(database=target.database, days=30, dry_run=False) %}
 
     {% set get_drop_commands_query %}
         select
@@ -40,35 +40,6 @@
         {% do log(schema_drop_command, True) %}
         {% if dry_run == 'false' %}
             {% do run_query(schema_drop_command) %}
-        {% endif %}
-    {% endfor %}
-
-{% endmacro %}
-{% macro clean_empty_schemas(database=target.database, dry_run=True) %}
-
-    {% set get_empty_schemas_query %}
-        select distinct
-            table_schema
-        from "{{ database }}".information_schema.tables
-        where table_schema not in ('DBT_PROD', 'SEEDS', 'RAW', 'SNAPSHOTS')
-        and table_schema not in (
-            select table_schema
-            from "{{ database }}".information_schema.tables
-            where table_type in ('EXTERNAL TABLE', 'BASE TABLE', 'VIEW')
-            group by 1
-        )
-    {% endset %}
-
-    {% set empty_schemas = run_query(get_empty_schemas_query).columns[0].values() %}
-
-    {% for empty_schema in empty_schemas %}
-        {% set drop_schema_command %}
-            DROP SCHEMA IF EXISTS "{{ empty_schema }}";
-        {% endset %}
-
-        {% do log('\nDropping empty schema: ' ~ empty_schema ~ '\n', info=True) %}
-        {% if dry_run == 'false' %}
-            {% do run_query(drop_schema_command) %}
         {% endif %}
     {% endfor %}
 
